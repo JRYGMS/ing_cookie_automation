cca2ba7341a7eea33b64b7b1a44aa7c1302526c4
Jarosław Gąsior <jaro21032001@wp.pl>
2026-06-13 15:01:42 +0200

    chore: add failure screenshots, increase timeout, and optimize page load strategy

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -5,7 +5,7 @@ def test_ing_cookies_acceptance(page: Page, context: BrowserContext):
 
     browser_name = page.context.browser.browser_type.name
     print(f'Uruchomiono test na przeglądarce: {browser_name.upper()}')
-    page.goto("https://www.ing.pl/")
+    page.goto("https://www.ing.pl/", wait_until="commit", timeout=60000)
 
     cookie_before = [cookie['name'] for cookie in context.cookies()]
     print(f'[BADANIE] Ciasteczka przed rozpoczęciem testu: {cookie_before}')
