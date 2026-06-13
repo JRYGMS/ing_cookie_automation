cbbe4a121c949e4698d8cc4c39c03376b119af45
Jarosław Gąsior <jaro21032001@wp.pl>
2026-06-13 16:13:04 +0200

    fix: update pytest configuration to run specific test file and enhance test logging

--- tests/test_site.py
+++ tests/test_site.py
@@ -6,5 +6,7 @@ def test_site_title(page: Page, context: BrowserContext):
     print(f'Uruchomiono test na przeglądarce: {browser_name.upper()}')
     
     page.goto("https://www.ing.pl/", wait_until="domcontentloaded", timeout=60000)
+    page.get_by_role("link", name="Załóż konto osobiste").click()
+    print(f'[BADANIE] Przejście do zakładki "Załóż konto osobiste" zakończone sukcesem.')
     title = page.title()
     print(f'[BADANIE] Tytuł strony: {title}')
