ea63bcb87ee412486ecf5b18195975f209bd3f69
Jarosław Gąsior <jaro21032001@wp.pl>
2026-06-13 21:23:55 +0200

    refactor: streamline cookie acceptance test and enhance error handling for Incapsula block

--- tests/confest.py
+++ tests/confest.py
@@ -1,5 +1,5 @@
 import pytest
-
+from playwright.sync_api import Page, BrowserContext
 @pytest.fixture(scope="session")
 
 def browser_context_args(browser_context_args):
@@ -9,4 +9,20 @@ def browser_context_args(browser_context_args):
         "locale": "pl-PL",
         "timezone_id": "Europe/Warsaw",
         "viewport": {"width": 1920, "height": 1080},
+        'extra_http_headers': {
+            "Accept-Language": "pl-PL,pl;q=0.9, en-US;q=0.8, en;q=0.7"
+        }
     }
+
+def check_incapsula_block(page: Page, context: BrowserContext):
+    page.goto("https://www.ing.pl/", wait_until="domcontentloaded", timeout=60000)
+    page.add_init_script("delete navigator.__proto__.webdriver;")
+    cookies = [cookie['name'] for cookie in context.cookies()]
+
+
+    try:
+        page.get_by_role('button', name='Dostosuj').wait_for(state='visible', timeout=20000)
+    except Exception:
+        if "incap_ses" in "".join(cookies) and not page.get_by_role('button', name='Dostosuj').is_visible():
+            print(f'[CI/CD BLOKADA] Imperva Incapsula zablokowała publiczny adres IP Github')
+            pytest.skip("Test pominięty z powodu blokady Imperva Incapsula")
--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -5,31 +5,16 @@ def test_ing_cookies_acceptance(page: Page, context: BrowserContext):
 
     browser_name = page.context.browser.browser_type.name
     print(f'Uruchomiono test na przeglądarce: {browser_name.upper()}')
-
-    page.add_init_script("delete navigator.__proto__.webdriver;")
-
     page.goto("https://www.ing.pl/", wait_until="domcontentloaded", timeout=60000)
 
     cookie_before = [cookie['name'] for cookie in context.cookies()]
     print(f'[BADANIE] Ciasteczka przed rozpoczęciem testu: {cookie_before}')
 
-    try:
-        page.get_by_role('button', name='Dostosuj').wait_for(state='visible', timeout=20000)
-    except Exception:
-        if "incap_ses" in "".join(cookie_before) and not page.get_by_role('button', name='Dostosuj').is_visible():
-            print(f'[CI/CD BLOKADA] Imperva Incapsula zablokowała publiczny adres IP Github')
-            pytest.skip("Test pominięty z powodu blokady Imperva Incapsula")                   
-            return 
-        else:
-            raise AssertionError("Nie znaleziono przycisku baneru cookies, mimo braku blokady firewall") 
+    
 
     
     page.get_by_role('button', name='Dostosuj').click()
-
-    
-    page.locator('div:nth-child(2) > .cookie-policy-switch > .cookie-policy-toggle-button > .cookie-policy-toggle-slider > .cookie-policy-slider-thumb').click()
-
-    
+    page.locator('div:nth-child(2) > .cookie-policy-switch > .cookie-policy-toggle-button > .cookie-policy-toggle-slider > .cookie-policy-slider-thumb').click()    
     page.get_by_role('button', name='Zaakceptuj zaznaczone').click()
     page.wait_for_timeout(1000)
 
@@ -41,9 +26,7 @@ def test_ing_cookies_acceptance(page: Page, context: BrowserContext):
     print(f'[BADANIE] Nowe ciasteczka analityczne: {new_cookies}')
 
     
-    assert len(all_cookies) > 0, "Nie zapisano jakichkolwiek ciasteczek w pamięci"
-
-    
+    assert len(all_cookies) > 0, "Nie zapisano jakichkolwiek ciasteczek w pamięci" 
     assert 'cookiePolicyGDPR' in cookie_names, "Nie znaleziono ciasteczka analitycznego cookiePolicyGDPR"
     assert 'cookiePolicyGDPR__details' in cookie_names, "Nie znaleziono szczegółów ciasteczka analitycznego cookiePolicyGDPR__details"
 
